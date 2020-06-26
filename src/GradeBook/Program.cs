7ba852c53e9f21a9b618ca7eb2ceedc72d3d3d9e
mahoryu <mahoryu@gmail.com>
2020-06-25 21:21:13 -0600

    learned foreach loop syntax

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,13 +7,25 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var grades = new List<double>() {12.7,10.3,6.11,4.11};
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach(double number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            
+            Console.WriteLine($"The average grade is {result}");
+
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
             }
             else
             {
-                Console.WriteLine("Hello!");
+                Console.WriteLine("Hello World!");
             }
         }
     }
